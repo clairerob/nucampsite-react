a2931b0e7f7d94184ae7f7374204a5a9c110c189
Claire Roberts <clairefinnie@gmail.com>
2022-09-26 13:15:21 +0100

    add featured items display cards to home page

---
+++ src/features/display/DisplayList.js
@@ -0,0 +1,23 @@
+import { Col, Row } from 'reactstrap';
+import DisplayCard from './DisplayCard';
+import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
+import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
+
+const DisplayList = () => {
+    const items = [selectFeaturedCampsite(), selectFeaturedPromotion()];
+//now the if that data ever changes in either data file, we don't have to change these files, we'll still have access to the current data
+    return (
+        <Row>
+            {items.map((item, idx) => {
+//React wants us to use a key with map, so use current index to generate that dynamically
+                return (
+                    <Col md className='m-1' key={idx}>
+                        <DisplayCard item={item} />
+                    </Col>
+                )
+            })}
+        </Row>
+    )    
+}
+
+export default DisplayList;
