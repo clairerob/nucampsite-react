b4a512685bf1b954ac747a3e4ad5c6242cdbccd7
Claire Roberts <clairefinnie@gmail.com>
2022-10-01 16:50:05 +0100

    workshop task 2+3. Add links and partnerSlice

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -2,9 +2,10 @@ import { Col, Row } from 'reactstrap';
 import DisplayCard from './DisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
+import { selectFeaturedPartner } from '../partners/partnersSlice';
 
 const DisplayList = () => {
-    const items = [selectFeaturedCampsite(), selectFeaturedPromotion()];
+    const items = [selectFeaturedCampsite(), selectFeaturedPromotion(), selectFeaturedPartner()];
 //now the if that data ever changes in either data file, we don't have to change these files, we'll still have access to the current data
     return (
         <Row>
